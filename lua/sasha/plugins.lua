56f174314cbc959e0fb6c8193685b098f5477151
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-07-27 10:10:10 -0400

    added impatient

--- lua/sasha/plugins.lua
+++ lua/sasha/plugins.lua
@@ -102,6 +102,9 @@ return packer.startup(function(use)
   -- Project manager
   use "ahmedkhalf/project.nvim"
 
+  -- Fast loading of shit
+  use 'lewis6991/impatient.nvim'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
