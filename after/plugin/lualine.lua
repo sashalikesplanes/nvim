5c76af3100f1727ee8a04843a608e18a84bf3602
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2023-04-09 17:23:19 +0200

    remove sections to save space

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -2,8 +2,8 @@ require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
     disabled_filetypes = {
       statusline = {},
       winbar = {},
@@ -17,29 +17,15 @@ require('lualine').setup {
       winbar = 1000,
     }
   },
-  sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
+  sections = {},
+  inactive_sections = {},
   tabline = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diagnostics', 'branch'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diagnostics', 'branch' },
+    lualine_c = { 'filename' },
+    lualine_x = { 'encoding', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
   },
   winbar = {},
   inactive_winbar = {},
