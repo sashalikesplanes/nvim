20b616ea10a96fcc76d8f64e9984253abb748614
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2023-04-17 10:37:57 +0200

    launch lsp in root of monorepo, and keybindigs

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,7 +6,8 @@ return {
   dependencies = {
     -- LSP Support
     { 'neovim/nvim-lspconfig' }, -- Required
-    { -- Optional
+    {
+      -- Optional
       'williamboman/mason.nvim',
       build = function()
         pcall(vim.cmd, 'MasonUpdate')
@@ -15,9 +16,9 @@ return {
     { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
     -- Autocompletion
-    { 'hrsh7th/nvim-cmp' }, -- Required
+    { 'hrsh7th/nvim-cmp' },     -- Required
     { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-    { 'L3MON4D3/LuaSnip' }, -- Required
+    { 'L3MON4D3/LuaSnip' },     -- Required
   },
   config = function()
     local lsp = require('lsp-zero').preset({})
@@ -35,6 +36,12 @@ return {
       nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
       nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
+      nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+      nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+      nmap('<leader>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+      end, '[W]orkspace [L]ist Folders')
+
       -- See `:help K` for why this keymap
       nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
       -- Conflicts with Tmux navigation
@@ -47,10 +54,15 @@ return {
       vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
         vim.lsp.buf.format()
       end, { desc = 'Format current buffer with LSP' })
-
     end)
 
-    require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+    local lspconfig = require('lspconfig')
+    lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
+    lspconfig.tsserver.setup {
+      cmd = { "typescript-language-server", "--stdio" },
+      filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+      root_dir = lspconfig.util.root_pattern('.git'),
+    }
 
     lsp.setup()
   end,
